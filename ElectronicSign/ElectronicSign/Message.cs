d6b76a7d2b82326ef602829f167c8462710ef6ca
Weasel <brenschild@gmail.com>
2018-01-15 03:01:45 -0500

    Code For Electronic Flip Sign GUI
    GUI

---
+++ ElectronicSign/ElectronicSign/Message.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElectronicSign
+{
+    class Message
+    {
+        public string Line1 { get; set; }
+        public string Line2 { get; set; }
+        public bool SingleLine { get; set; }
+        public bool Scrolling { get; set; }
+        public string ScrollingSpeed { get; set; }
+        public string DisplayTime { get; set; }
+    }
+}
